afb5dcebe2576556a21c8e53e8ee054aa77ebfb6
arief sangga <arif.sangga@gmail.com>
2023-03-21 11:18:29 +0700

    update kendala duitku pembayaran

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,23 +41,25 @@ class LoginController extends Controller
 
             $tahun = date('Y');
             $cektransaction = Transaction::where('user_id', auth()->user()->id)
-                    ->where('tahun', $tahun)->first();
+                ->where('tahun', $tahun)->first();
             $cekcart = Cart::where('user_id', auth()->user()->id)
-                    ->where('tahun', $tahun)->first();
-            
+                ->where('tahun', $tahun)->first();
+
 
             // kondisi jika di tabel transcation dan cart ada user dan tahun maka tidak insert
             if (!$cektransaction && !$cekcart) {
                 Cart::insert([
                     'user_id'       => auth()->user()->id,
                     'product_id'    => 1,
-                    'product_image'    => 'R3nBQIQbLmjkb1KRCNhQhmcYa8S5d8OCGLoChpVD.png',
+                    'product_image'    => 'gMreGxufmxSztWIZGMmFy8wCsy9rIdxvjCH2uj2M.png',
+                    'size'    => 'Iuran',
+                    'qty'    => 1,
                     'price'         => 50000,
                     'tahun'           => $tahun,
                     'weight'        => $request->weight
                 ]);
             }
-           
+
 
             //redirect route dashboard
             return redirect()->route('account.dashboard');
