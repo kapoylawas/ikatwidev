cef8e3e7126da03c1222e2bfbf759fafe989f62d
arief sangga <arif.sangga@gmail.com>
2023-03-21 10:29:57 +0700

    update build

--- public/build/assets/Index.30648e76.js
+++
@@ -1,1 +0,0 @@
-import{R as e,H as t}from"./app.4c832ae2.js";import{L as a}from"./Web.67efaa85.js";import"./index.affde227.js";function m(){return e.createElement(e.Fragment,null,e.createElement(t,null,e.createElement("title",null,"IKATWI - Ikatan Terapis Wicara")),e.createElement(a,null,"Artikel"))}export{m as default};
