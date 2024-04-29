9d7c3c9de8fb65c03b2baea6a2b588a55cc19af3
arief sangga <arief.heaven@gmail.com>
2024-04-29 11:09:17 +0700

    update wrong

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,7 +17,7 @@
 </head>
 
 <body class="hold-transition sidebar-mini">
-  s
+  
     @inertia
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
