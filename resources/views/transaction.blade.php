b045bb95e225fe75521e0ff3a0650af132bdeac0
arief sangga <arief.heaven@gmail.com>
2024-05-02 08:23:22 +0700

    update exportt excl

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -2,20 +2,22 @@
 <table>
     <tr>
       <th>Nama</th>
-      <th>Grand Total</th>
-      <th>Status</th>
       <th>NO ekta</th>
       <th>DPW</th>
-      <th>Tanggal Transaction</th>
+      <th>DPC</th>
+      <th>Nominal Bayar</th>
+      <th>Tanggal Bayar</th>
+      <th>Status Pembayaran</th>
     </tr>
     @foreach ($data as $ts )
     <tr>
         <td>{{ $ts->user->name }}</td>
-        <td>{{ $ts->grand_total }}</td>
-        <td>{{ $ts->status }}</td>
         <td>{{ $ts->user->no_anggota }}</td>
         <td>{{ $ts->province->name }}</td>
+        <td>{{ $ts->city->name }}</td>
+        <td>{{ $ts->grand_total }}</td>
         <td>{{ $ts->created_at }}</td>
+        <td>{{ $ts->status }}</td>
     </tr>
     @endforeach
 </table>
