d97ede87ae5bafce74d5828d773e393d8657cdda
kapoylawas <arif.sangga@gmail.com>
2025-02-10 13:07:59 +0700

    update build perjaan per dpw

---
+++ public/build/assets/Index.c4e460d8.js
@@ -0,0 +1,1 @@
+import{R as e,H as t}from"./app.f3f3e2a7.js";import{L as a}from"./Web.131ae3fd.js";import"./Dropdown.fd423020.js";import"./index.352e37cf.js";function i(){return e.createElement(e.Fragment,null,e.createElement(t,null,e.createElement("title",null,"IKATWI - Ikatan Terapis Wicara")),e.createElement(a,null))}export{i as default};
--- public/build/assets/Index.f4b6f3ed.js
+++
@@ -1,1 +0,0 @@
-import{R as e,H as t}from"./app.5dc1fe0b.js";import{L as a}from"./Web.281fac89.js";import"./Dropdown.99ca5f6a.js";import"./index.8ad21ee9.js";function i(){return e.createElement(e.Fragment,null,e.createElement(t,null,e.createElement("title",null,"IKATWI - Ikatan Terapis Wicara")),e.createElement(a,null,"Artikel"))}export{i as default};
