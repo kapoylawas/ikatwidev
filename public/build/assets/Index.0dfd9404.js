245b88de5b97d3b51ba533ca1d3dce7a5d086fed
kapoylawas <arif.sangga@gmail.com>
2025-02-10 09:44:48 +0700

    update build total user aktif

---
+++ public/build/assets/Index.0dfd9404.js
@@ -0,0 +1,1 @@
+import{R as e,H as t}from"./app.f67f340a.js";import{L as a}from"./Web.07c6bb4f.js";import"./Dropdown.959fbfc2.js";import"./index.268ff48c.js";function i(){return e.createElement(e.Fragment,null,e.createElement(t,null,e.createElement("title",null,"IKATWI - Ikatan Terapis Wicara")),e.createElement(a,null))}export{i as default};
